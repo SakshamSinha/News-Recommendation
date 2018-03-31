2a6a6ab3e7c30f6dff275e58230ebea58f694107
neenurose <neenuvincent1992@gmail.com>
2018-03-31 12:33:03 +0100

    update user peferences in db

--- newsrecoapp/application/views.py
+++ newsrecoapp/application/views.py
@@ -18,7 +18,7 @@ from django.core import serializers
 from application import dbops
 from application import forms
 from application import models
-from application.models import NewsModel, NewsProfileModel
+from application.models import NewsModel, NewsProfileModel,UserStaticPrefs
 
 import json
 
@@ -140,10 +140,11 @@ class AjaxPosts(TemplateView):
 
 	def update_relevance(request):
 		if (request.is_ajax):
+			print(request.user)
 			message = "success"
 			news_id = request.POST.get('news_id')
 			relevance = request.POST.get('relevance')
-			news_profile_object = NewsProfileModel.objects.filter(news=news_id).first()
+			news_profile_object = NewsProfileModel.objects.filter(user=request.user,news=news_id).first()
 			if news_profile_object is None:
 				news_model_object = NewsModel.objects.get(id=news_id)
 				news_profile_object = NewsProfileModel.objects.create(user=request.user, news=news_model_object,
@@ -151,7 +152,55 @@ class AjaxPosts(TemplateView):
 			else:
 				news_profile_object.relevance = relevance
 			news_profile_object.save()
+			# updating score
+			print(news_profile_object.show_more)
+			print(news_profile_object.relevance)
+			update_user_prefs(request.user,news_profile_object.show_more,news_profile_object.relevance,news_profile_object.news.categories)
 
 		else:
 			message = "fail"
 		return HttpResponse(message)
+
+def update_user_prefs(user, show_more, relevance, category):
+	user_prefs_object = UserStaticPrefs.objects.filter(profileof_user=user).first()
+	weight_TT = 0.9
+	weight_TF = 0.5
+	weight_FF = 0.1
+	if int(show_more) == 1 and int(relevance) == 1:
+		print("inside if")
+		user_prefs_object = select_category(user_prefs_object, category, weight_TT)
+	elif int(show_more) == 0 and int(relevance) == 0:
+		print("inside elif")
+		user_prefs_object = select_category(user_prefs_object, category, weight_FF)
+	else:
+		print("inside else")
+		user_prefs_object = select_category(user_prefs_object, category, weight_TF)
+	user_prefs_object.save()
+
+def select_category(user_prefs_object, category, weight):
+	if user_prefs_object is None:
+		if "economy" in category:
+			user_prefs_object = UserStaticPrefs.objects.create(profileof_user=user, dy_economy=weight)
+		if "politics" in category:
+			user_prefs_object = UserStaticPrefs.objects.create(profileof_user=user, dy_politics=weight)
+		if "science" in category:
+			user_prefs_object = UserStaticPrefs.objects.create(profileof_user=user, dy_science=weight)
+		if "arts" in category:
+			user_prefs_object = UserStaticPrefs.objects.create(profileof_user=user, dy_arts=weight)
+		if "sport" in category:
+			user_prefs_object = UserStaticPrefs.objects.create(profileof_user=user, dy_sports=weight)
+	else:
+		if "economy" in category:
+			user_prefs_object.dy_economy = user_prefs_object.dy_economy + weight
+		if "politics" in category:
+			user_prefs_object.dy_politics = user_prefs_object.dy_politics + weight
+		if "science" in category:
+			user_prefs_object.dy_science = user_prefs_object.dy_science + weight
+		if "arts" in category:
+			user_prefs_object.dy_arts = user_prefs_object.dy_arts + weight
+		if "sport" in category:
+			user_prefs_object.dy_sports = user_prefs_object.dy_sports + weight
+	return user_prefs_object
+
+
+
